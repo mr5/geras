0fcf0663ed4e8695185fa9b357cb0b2c18d6ddc3
mr.5 <u@mr-5.cn>
2014-03-09 21:18:13 +0800

    上次提交时忘记一些文件

--- src/js/demeter-demo.js
+++ src/js/demeter-demo.js
@@ -3,13 +3,16 @@
  */
 KISSY.ready(function(){
     KISSY.use('uri,node', function(S, Uri, Node){
+        // 设置顶部菜单高亮
         var current_page_url = document.location.href;
         current_page_url = current_page_url.substr(current_page_url.indexOf('tests/') + 6);
         var active = current_page_url.substring(0, current_page_url.indexOf('.html'));
-//        var query = new Uri.Query(queryStr);
-//        var active = query.get('active');
         if(active != null) {
             Node.one('#J_nav_' + active).addClass('active');
         }
+
+        // 代码高亮渲染
+        Node.all('pre').addClass('prettyprint');
+        prettyPrint();
     });
 });
